346250d6b9b529c790dc0fbdaf9611f216689ce0
Alexander Pokidin <a.pokidin@gmail.com>
2019-08-28 14:16:42 +0300

    ChatAdapter added

--- app/src/main/java/ru/skillbranch/devintensive/ui/main/MainActivity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/ui/main/MainActivity.kt
@@ -1,13 +1,17 @@
 package ru.skillbranch.devintensive.ui.main
 
+
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_main.*
 import ru.skillbranch.devintensive.R
 import ru.skillbranch.devintensive.viewmodels.MainViewModel
-
 class MainActivity : AppCompatActivity() {
 
+    private val chatAdapter: RecyclerView.Adapter<RecyclerView.ViewHolder>?
     private lateinit var viewModel: MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,13 +23,18 @@ class MainActivity : AppCompatActivity() {
         initViewModel()
     }
 
-    private fun initViewModel() {
-        viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
+    private fun initTools() {
+        setSupportActionBar(toolbar)
     }
 
     private fun initModels() {
+        with(rv_chat_list){
+            adapter = chatAdapter
+            layoutManager = LinearLayoutManager(this@MainActivity)
+        }
     }
 
-    private fun initTools() {
+    private fun initViewModel() {
+        viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
     }
 }
