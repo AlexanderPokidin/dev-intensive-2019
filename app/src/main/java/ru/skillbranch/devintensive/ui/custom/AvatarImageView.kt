4eede6c146ee1222b5151bc3f6243a7f6d046884
Alexander Pokidin <a.pokidin@gmail.com>
2019-08-29 15:09:29 +0300

    Branch hometask_5 created

--- app/src/main/java/ru/skillbranch/devintensive/ui/custom/AvatarImageView.kt
+++ app/src/main/java/ru/skillbranch/devintensive/ui/custom/AvatarImageView.kt
@@ -2,11 +2,13 @@ package ru.skillbranch.devintensive.ui.custom
 
 import android.content.Context
 import android.util.AttributeSet
-import android.widget.ImageView
 
 class AvatarImageView @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
     defStyleAttr: Int = 0
-) : ImageView(context, attrs, defStyleAttr) {
+) : CircleImageView(context, attrs, defStyleAttr) {
+    fun setInitials(initials: String) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 }
